54b8e7ec6a5fb4b9b375408cc5e9d168f80b56f3
Manel-93 <159416474+Manel-93@users.noreply.github.com>
2024-02-08 09:50:20 +0100

    Add files via upload

---
+++ controllers/controller.php
@@ -0,0 +1,16 @@
+<?php
+switch ($action)
+{
+    case 'reponse':
+        {
+            //var_dump($_REQUEST['liste']);
+            $liste=$_REQUEST['liste'];
+           
+            include 'views/reponse.php';
+            break;
+        }
+    default:
+        {include 'views/choix.php';}
+
+
+}
